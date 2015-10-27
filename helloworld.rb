948438dacc3cade53b7b3be7d3cf7dd5da93ed80
Andrew Weaver <weevz@uw.edu>
2015-10-27 16:52:29 -0700

    Update helloworld.rb

--- helloworld.rb
+++ helloworld.rb
@@ -7,11 +7,13 @@ class Helloworld < Formula
   sha256 "b678e63e3586070031d3cc7231bb2c24dcd17484ecd14fdb70c255a1ac62b7de"
   
   depends_on "ffmpeg" => [ 'with-ffplay', 'with-libass', 'with-freetype', 'with-faac' ]
+  depends_on 'mediainfo'
 
 
  
   def install
     bin.install 'helloworld'
+    bin.install 'dvtest'
    
   end
 end
