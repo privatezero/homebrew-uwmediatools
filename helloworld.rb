7db52c8d4952d9224af4278cace0cf28603e6ff4
A Weaver <weevz@uw.edu>
2015-10-27 11:08:06 -0700

    helloworld.rb

---
+++ helloworld.rb
@@ -0,0 +1,15 @@
+
+class Helloworld < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/privatezero/uwmediatools/archive/1.0.2.zip"
+  version "1.0"
+  sha256 "1db0762a575e630ef2fc63993a6a7142267f2345b8efad22ed5ce8e0f4e0626f"
+
+
+ 
+  def install
+    bin.install 'helloworld'
+   
+  end
+end
