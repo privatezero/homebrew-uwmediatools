7de555c193aa9ca95851ed4bf0f64575ee9b10e2
Andrew Weaver <weevz@uw.edu>
2015-10-27 13:16:43 -0700

    Update helloworld.rb

--- helloworld.rb
+++ helloworld.rb
@@ -2,7 +2,7 @@
 class Helloworld < Formula
   desc ""
   homepage ""
-  url "https://github.com/privatezero/uwmediatools/archive/1.0.2.zip"
+  url "https://github.com/privatezero/uwmediatools/archive/1.0.3.zip"
   version "1.0"
   sha256 "1db0762a575e630ef2fc63993a6a7142267f2345b8efad22ed5ce8e0f4e0626f"
   
