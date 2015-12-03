ecb7e0bf1654fc735876166ceefdaa64ec89d077
Andrew Weaver <weevz@uw.edu>
2015-12-03 11:23:34 -0800

    Update helloworld.rb

--- helloworld.rb
+++ helloworld.rb
@@ -2,13 +2,14 @@
 class Helloworld < Formula
   desc ""
   homepage ""
-  url "https://github.com/privatezero/uwmediatools/archive/1.1.2.zip"
-  version "1.1.2"
+  url "https://github.com/privatezero/uwmediatools/archive/1.1.3.zip"
+  version "1.1.3"
   sha256 "d865ba0d0572fd89e9a5b4b19f08da2449e8e166504e28ddc340d7d09f2d0c6f"
   
   depends_on "ffmpeg" => [ 'with-ffplay', 'with-libass', 'with-freetype', 'with-faac' ]
   depends_on 'cask'
   depends_on 'mediainfo'
+  depends_on 'bagit'
 
 
  
