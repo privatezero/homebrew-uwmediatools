543df679c1999e8b6ee3bc6c9ea4b379fe52898c
Andrew Weaver <weevz@uw.edu>
2016-02-26 11:13:22 -0800

    Update and rename helloworld.rb to uwmediatools.rb

--- uwmediatools.rb
+++ uwmediatools.rb
@@ -1,9 +1,9 @@
 
-class Helloworld < Formula
+class Uwmediatools < Formula
   desc ""
   homepage ""
-  url "https://github.com/privatezero/uwmediatools/archive/1.1.6.zip"
-  version "1.1.6"
+  url "https://github.com/privatezero/uwmediatools/archive/1.1.7.zip"
+  version "1.1.7"
   sha256 "5ad6874b24c61b204ab748ccc7344b94bf43484a70e246facebda9cc4bfde384"
   
   depends_on "ffmpeg" => [ 'with-ffplay', 'with-libass', 'with-freetype', 'with-faac' ]
