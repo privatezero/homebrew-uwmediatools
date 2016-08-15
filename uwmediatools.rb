fc02099df0e49a595e03cc96f8306eb628caef38
Andrew Weaver <weevz@uw.edu>
2016-08-15 16:48:20 -0400

    Update uwmediatools.rb

--- uwmediatools.rb
+++ uwmediatools.rb
@@ -2,9 +2,9 @@
 class Uwmediatools < Formula
   desc ""
   homepage ""
-  url "https://github.com/privatezero/uwmediatools/archive/1.3.1.zip"
-  version "1.3.1"
-  sha256 "07df72ab805ceb89697f9a7ac06a97b5dc9675d6924a206c7ae8cff2f8d7baa5"
+  url "https://github.com/privatezero/uwmediatools/archive/1.3.2.zip"
+  version "1.3.2"
+  sha256 "556fc4da680d80b7a069fb8026bdb7b9e70781570110aa455fc69d46d602cbac"
   
   depends_on "ffmpeg" => [ 'with-ffplay', 'with-libass', 'with-freetype', 'with-faac' ]
   depends_on 'cask'
