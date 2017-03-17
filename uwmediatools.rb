83d0484923d572f83ba6f23cc62d585f8c9277f3
Andrew Weaver <weevz@uw.edu>
2017-03-17 10:11:02 -0400

    Update uwmediatools.rb

--- uwmediatools.rb
+++ uwmediatools.rb
@@ -4,6 +4,8 @@ class Uwmediatools < Formula
   homepage ""
   url "https://github.com/privatezero/uwmediatools/archive/1.4.4.zip"
   version "1.4.4"
+  revision 1
+  
   sha256 "cb97eb5121d4808ba3f536959b78fd36a9380f41246ca6a1726783b14be8cde8"
   depends_on 'cask'
   depends_on 'mediainfo'
